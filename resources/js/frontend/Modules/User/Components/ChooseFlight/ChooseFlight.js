1745d19292cca4306b9451e5bb0586f57602cae2
mylaptop <hieuvanphan275@gmail.com>
2021-08-07 09:23:21 +0700

    update

--- resources/js/frontend/Modules/User/Components/ChooseFlight/ChooseFlight.js
+++ resources/js/frontend/Modules/User/Components/ChooseFlight/ChooseFlight.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Component } from "react";
 import SubNavbar from "../../Shared/Components/SubNavbar/SubNavbar";
+import SideBarFilter from "./Components/SideBarFilter/SideBarFilter";
 import StepListBar from "./Components/StepList/StepList";
+import "./ChooseFlight.scss";
+import BookingTicketList from "./Components/BookingTicketList/BookingTicketList";
 
 class ChooseFlight extends Component {
     constructor(props) {
@@ -16,6 +19,16 @@ class ChooseFlight extends Component {
                 <div className="choose-flight-page">
                     <div className="wrap-container">
                         <StepListBar />
+                        <div className="main-content">
+                            <div className="row">
+                                <div className="col-sm-4">
+                                    <SideBarFilter />
+                                </div>
+                                <div className="col-sm-8">
+                                    <BookingTicketList />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ resources/js/frontend/Modules/User/Components/ChooseFlight/Components/SideBarFilter/SideBarFilter.js
@@ -0,0 +1,23 @@
+import { Typography } from "@material-ui/core";
+import React from "react";
+import { Component } from "react";
+import "./SideBarFilter.scss";
+
+class SideBarFilter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>
+               <div className="side-bar-filter">
+                    
+               </div>
+            </div>
+        );
+    }
+}
+
+export default SideBarFilter;
